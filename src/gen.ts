85d3caeb4331a513c66de6c97004407e509c90e0
Marcus S. Abildskov <msa@siteattention.com>
2019-08-12 17:10:40 +0200

    refactor(): remove unnecessary code

--- src/gen.ts
+++ src/gen.ts
@@ -2,7 +2,7 @@ import * as execa from 'execa';
 import * as glob from 'fast-glob';
 import { join } from 'path';
 
-import { Options, Generator } from './interfaces';
+import { Options } from './interfaces';
 import { createGenerator } from './generator/create';
 
 // Usage: <language> <istio-api-repo-url> <istio-api-path> <template-dir> <out-dir>
@@ -21,41 +21,16 @@ import { createGenerator } from './generator/create';
     outDir,
     sdk,
   } as Options;
-  // console.log('Emptying Istio API...');
-  await execa('rm', ['-rf', options.istioApiPath]);
 
-  // console.log('Cloning Istio API...');
+  await execa('rm', ['-rf', options.istioApiPath]);
   await execa('git', ['clone', options.istioApiRepo, options.istioApiPath]);
 
-  // console.log('Globbing all JSON files');
   options.jsonPaths = await glob(['**/*.json'], {
     cwd: options.istioApiPath,
   });
 
   const generator = await createGenerator(options.sdk);
   await generator.generate(options);
-
-  /*const outputTypes = groups.map(({ schemaConfigs: schemas }) => {
-    const template = readFileSync(join(templateDir, 'typesTest.ts.mustache')).toString();
-    return mustache.render(template, { schemas });
-    // writeFileSync(join(outDir, 'types', 'output.ts'), outputTypes, 'utf8');
-    // process.exit();
-    schemaConfigs.forEach(kind => {
-      const template = readFileSync(join(templateDir, 'typesTest.ts.mustache')).toString();
-
-      const kindTs = mustache.render(template, {
-        ...kind,
-        version,
-        group,
-      });
-
-      writeFileSync(join(outDir, group, version, paramCase(kind.kind) + '.ts'), kindTs, 'utf8');
-
-      // console.log(kind.properties);
-    })
-  });
-
-  writeFileSync(join(outDir, 'types', 'output.ts'), outputTypes.join(`\r\n`), 'utf8');*/
 
   await execa('rm', ['-rf', istioApiPath]);
 })();
