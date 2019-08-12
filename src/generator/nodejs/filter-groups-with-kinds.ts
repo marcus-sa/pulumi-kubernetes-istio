8d05f0bcd14fd49599d3b3a09e52092c300f1785
Marcus S. Abildskov <marcus-sa@outlook.com>
2019-08-12 21:27:29 +0200

    refactor(generator): filter groups with kinds

--- src/generator/nodejs/filter-groups-with-kinds.ts
+++ src/generator/nodejs/filter-groups-with-kinds.ts
@@ -2,30 +2,16 @@ import { GroupTS } from './interfaces';
 
 export function filterGroupsWithKinds(groups: GroupTS[]): GroupTS[] {
   return groups.reduce((groupsWithKinds, { group, versions }) => {
-    if (versions.some(({ schemas }) => schemas.some(({ isKind }) => isKind))) {
-      const groupIndex =
-        groupsWithKinds.push({
-          group,
-          versions: [],
-        }) - 1;
+    versions = versions.reduce((versionsWithKinds, { version, schemas }) => {
+      schemas = schemas.filter(({ isKind }) => isKind);
 
-      for (const { version, schemas } of versions) {
-        if (schemas.some(({ isKind }) => isKind)) {
-          const versionIndex =
-            groupsWithKinds[groupIndex].versions.push({
-              version,
-              schemas: [],
-            }) - 1;
+      return schemas.length > 0
+        ? [...versionsWithKinds, { version, schemas }]
+        : versionsWithKinds;
+    }, []);
 
-          for (const schema of schemas) {
-            if (schema.isKind) {
-              groupsWithKinds[groupIndex].versions[versionIndex].schemas.push(schema);
-            }
-          }
-        }
-      }
-    }
-
-    return groupsWithKinds;
+    return versions.length > 0
+      ? [...groupsWithKinds, { group, versions }]
+      : groupsWithKinds;
   }, []);
 }
