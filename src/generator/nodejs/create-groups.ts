b52fbc57aa5b49389fd5bebc79feccec29b7d765
Marcus S. Abildskov <marcus-sa@outlook.com>
2019-08-12 21:36:22 +0200

    refactor(generator): create groups

--- src/generator/nodejs/create-groups.ts
+++ src/generator/nodejs/create-groups.ts
@@ -1,4 +1,6 @@
-import { GroupTS, GroupVersions } from './interfaces';
+import deepmerge = require('deepmerge');
+
+import { GroupTS, GroupVersions, VersionTS } from './interfaces';
 import { getDefinitions } from './get-definitions';
 import { createSchemas } from './create-schemas';
 
@@ -8,32 +10,26 @@ export async function createGroups(
   paths: string[],
   istioApiPath: string,
 ): Promise<GroupTS[]> {
-  const groups: GroupTS[] = [];
-  for (const { group, versions } of groupVersions) {
-    const groupIndex =
-      groups.push({
-        group,
-        versions: [],
-      }) - 1;
-
-    for (const version of versions) {
+  return Promise.all(groupVersions.map(async ({ group, versions }) => {
+    const groupVersions: VersionTS[] = await Promise.all(versions.map(async (version) => {
       const groupVersionPaths = paths.filter(path => path.includes(`${group}/${version}`));
       const definitions = await getDefinitions(groupVersionPaths, istioApiPath);
 
-      const versionIndex =
-        groups[groupIndex].versions.push({
-          version,
-          schemas: [],
-          inputs: [],
-        }) - 1;
+      const initial: VersionTS = {
+        version,
+        schemas: [],
+        inputs: [],
+      };
 
-      for (const definition of definitions) {
-        const { schemas, inputs } = groups[groupIndex].versions[versionIndex];
-        schemas.push(...createSchemas(definition, false));
-        inputs!.push(...createSchemas(definition, true));
-      }
-    }
-  }
+      return definitions.reduce((version, definition) =>
+        deepmerge(version, {
+          schemas: createSchemas(definition, false),
+          inputs: createSchemas(definition, true),
+        } as VersionTS),
+        initial,
+      );
+    }));
 
-  return groups;
+    return { group, versions: groupVersions };
+  }));
 }
